1b6350710ee5f0ef1de8b40e283c999ca723d742
Bui Xuan Binh <buixuanb187@gmail.com>
2026-03-21 17:05:56 +0700

    add-dashboard

--- views/main.php
+++ views/main.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title><?= $title ?? 'Home' ?></title>
+    <title>FinCore System - <?= $title ?? 'Dashboard' ?></title>
 
     <!-- Latest compiled and minified CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
@@ -19,7 +19,13 @@
     <nav class="navbar navbar-expand-xxl bg-light justify-content-center">
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link text-uppercase" href="<?= BASE_URL ?>"><b>Home</b></a>
+                <a class="nav-link text-uppercase" href="<?= BASE_URL ?>"><b>Dashboard</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-uppercase" href="<?= url('branches') ?>"><b>Chi nhánh</b></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-uppercase" href="<?= url('employees') ?>"><b>Nhân viên</b></a>
             </li>
         </ul>
     </nav>
